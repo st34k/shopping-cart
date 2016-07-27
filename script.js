2a203782d90c6e68b1bc5bb853cc8726b9070ccd
Sashko <Sashko>
2016-07-27 19:53:15 +0300

    added localstorage

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 // an array with all of our cart items
+
 var total = 0;
+var STORAGE_ID = 'shoppingcart';
+// var total = 0;
+var addFromStorage = Number(localStorage.getItem('tot'));
+
+if (addFromStorage !== 0){
+  var total= addFromStorage;
+}
+
+var getFromLocalStorage = function () {
+    return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+  }
+
+
 var cart = {
-  items : []
+  items : getFromLocalStorage()
   
+  };
+
+
+
+var saveToLocalStorage = function () {
+  localStorage.setItem(STORAGE_ID, JSON.stringify(cart.items));
   };
 
 var updateCart = function () {
@@ -16,15 +36,28 @@ $('.cart-list').append(newHTML);
 
 
 var addItem = function (item,price) {
+
   var item = {
     item:item,
     price:price
   };
-
-  total+=item.price;
+   
+  total += item.price;
   cart.items.push(item); 
+  
 
+  if (total !== 0){
+    localStorage.setItem('tot', total);
+
+  }
+
+
+  // for (var i = 0; i<cart.items.length ; i++){
+  //   total += cart.items[i].price;
+  // }
+  // var showTotal = total + addFromStorage;
   document.getElementById('total').innerHTML = total;
+  saveToLocalStorage();
  
   
 }
@@ -34,24 +67,30 @@ var clearCart = function () {
   cart.items=[];
   $('.cart-list').empty();
   total = 0;
+  localStorage.setItem('tot', total);
+
+
   document.getElementById('total').innerHTML = total;
+
+  saveToLocalStorage();
   
   
 }
 
 
-var removeItem = function(removeitem){
-  // console.log($(removeitem).closest('.added-item'));
-  var $toRemove = $(removeitem).closest('.added-item');
-  $(removeitem).closest('.added-item').remove();
-  // var index = $.inArray($toRemove.val());
-  console.log(index);
+// var removeItem = function(removeitem){
+//   // console.log($(removeitem).closest('.added-item'));
+//   var $toRemove = $(removeitem).closest('.added-item');
+//   $(removeitem).closest('.added-item').remove();
+//   // var index = $.inArray($toRemove.val());
+//   console.log(index);
 
 
-}
+// }
 
 $('.view-cart').on('click', function () {
   $('.shopping-cart').toggleClass('show');
+  document.getElementById('total').innerHTML = localStorage.getItem('tot');
 });
 
 
@@ -60,6 +99,7 @@ $('.add-to-cart').on('click', function () {
   var item = $(this).closest('.card').data().name;
   var price = $(this).closest('.card').data().price;
   // var itemPrice = "  $"+price;
+
   addItem(item,price);
   updateCart();
 });
@@ -73,10 +113,10 @@ $('.shopping-cart').on('click','.clear-cart', function(){
 
 });
 
-$(".cart-list").on('click','.remove-item', function(){
-  removeItem(this);
+// $(".cart-list").on('click','.remove-item', function(){
+//   removeItem(this);
 
-});
+// });
 
 // update the cart as soon as the page loads!
 updateCart();
